396af7a0087d014397d6b29132e5494734d9aab7
TomEwbank <tom.ewbank@student.ulg.ac.be>
2015-05-06 21:58:25 +0200

    Fix for add_alloc

--- sources/testalloc.py
+++ sources/testalloc.py
@@ -12,17 +12,19 @@ def add_alloc(s):
 			
 		if rest[i-5:i] != "label":
 			matchObj = re.match(r'[a-zA-Z_][a-zA-Z_0-9]*', rest[i+1:], re.I)
-			j = len(matchObj.group())
-			print(j)
-			varName = rest[i:i+j+1]
-			
-			k = i
-			char = rest[k]
-			while char != '\n':
-				k -= 1
+			if matchObj == None:
+				result = result + rest[0:i+1]
+			else :
+				j = len(matchObj.group())
+				varName = rest[i:i+j+1]
+				
+				k = i
 				char = rest[k]
-			
-			result = result + rest[0:k+1] + varName + " = alloca undef " + rest[k:i+1]
+				while char != '\n':
+					k -= 1
+					char = rest[k]
+				
+				result = result + rest[0:k+1] + varName + " = alloca undef " + rest[k:i+1]
 		else:
 			result = result + rest[0:i+1]
 			
@@ -30,6 +32,6 @@ def add_alloc(s):
 		
 	return result
 	
-str = "xxxxxxxxxx\nxxxxxxxxx\nxxxxx %bouBi85fs, xxx\nxxxxxxx\nxxxx %i) xxxx\nxxxxxxxxxx"
+str = "xxxxxxxxxx\nxxxxxxxxx\nxxxxx %bouBi85fs, xxx\nxxxxxxx\nxxxx %i) xxxx\nxxxx%1xxxxxx"
 str = add_alloc(str)
 print(str)
